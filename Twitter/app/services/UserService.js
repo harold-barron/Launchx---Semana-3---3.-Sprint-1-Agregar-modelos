9a38f84e9850215d0ddd89ad7816055f268b5512
Harold Barron <haroldbarron13@gmail.com>
2022-04-26 17:59:28 -0500

    Adding the necesary code to past the second test

--- Twitter/app/services/UserService.js
+++ Twitter/app/services/UserService.js
@@ -1,3 +1,4 @@
+const user = require("./../models/User")
 const User = require("./../models/User")
 
 class UserService 
@@ -5,6 +6,12 @@ class UserService
     static create(id,username,name)
     {
        return new User(id,username,name,"Sin bio")
+    }
+
+    static getInfo(user)
+    {
+        const userInList= [user.id,user.username,user.name,user.bio]
+        return  userInList
     }
 }
 
--- Twitter/test/service/UserService.test.js
+++ Twitter/test/service/UserService.test.js
@@ -11,12 +11,13 @@ describe ('Unit test for User Service class', () =>
         expect(User2.username).toBe('username')
         expect(User2.name).toBe('name')
         expect(User2.bio).not.toBeUndefined()
-        
+        console.log(User2)
     });
     test('2)Get all user data in a list ', () =>
     {
         const user2 = UserService.create(1,"haroldvanlecks","harold")
         const userInfoInList = UserService.getInfo(user2)
+        
         expect(userInfoInList[0]).toBe(1)
         expect(userInfoInList[1]).toBe("haroldvanlecks")
         expect(userInfoInList[2]).toBe("harold")
