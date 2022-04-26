54d7d9f898bc33a7f2dc4ed08daea9d879b7b5ce
Harold Barron <haroldbarron13@gmail.com>
2022-04-25 22:40:41 -0500

    Solution first requierment

--- Twitter/test/service/UserService.test.js
+++ Twitter/test/service/UserService.test.js
@@ -5,12 +5,12 @@ describe ('Unit test for User Service class', () =>
 {
     test('1) Creating a new user with the UserService class', ()=>
     {
-        const User2 = new UserService(1,'username','name')
+        const User2 =UserService.create(1,'username','name')
 
         expect(User2.id).toBe(1)
         expect(User2.username).toBe('username')
         expect(User2.name).toBe('name')
         expect(User2.bio).not.toBeUndefined()
         
-    }) 
+    });
 })
