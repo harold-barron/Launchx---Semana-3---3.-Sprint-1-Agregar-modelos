5764a17eabb138d6b238f0418a4f4e4c3562799d
Harold Barron <haroldbarron13@gmail.com>
2022-04-26 19:22:00 -0500

    Adding test n° 3  and passing it

--- Twitter/app/views/UserView.js
+++ Twitter/app/views/UserView.js
@@ -16,7 +16,12 @@ class UserView
             result.error = `nesecitan tener un valor valido`
             return result
         }
-        
+        // if (playload.username==="username" && playload.name === "name" && playload.id ===12)
+        // {
+        //     let result = new Object()
+        //     result.error = `valor valido`
+        //     return result
+        // }
     }
 }
 module.exports = UserView
--- Twitter/test/views/UserView.test.js
+++ Twitter/test/views/UserView.test.js
@@ -2,16 +2,23 @@ const userView = require("./../../app/views/UserView")
 
 describe("Test for UserView", () =>{
 
-    // test("1.Return an error object when try to create a new user with a null playload", () =>
-    // {
-    //     const playload = null
-    //     const result = userView.createUser(playload)
-    //     expect(result.error).toMatch(/playload no existe/)
-    // })
+    test("1.Return an error object when try to create a new user with a null playload", () =>
+    {
+        const playload = null
+        const result = userView.createUser(playload)
+        expect(result.error).toMatch(/playload no existe/)
+    })
     test("2. Return an error object when try to create a new user with a playload with invalid properties", () =>
     {
         const playload = {username:null, name:12,id:"id"}
         const result = userView.createUser(playload)
         expect(result.error).toMatch(/nesecitan tener un valor valido/)
+    })
+    test("3. Return an error object when try to create a new user with a playload with missing properties", () =>
+    {
+        const playload = {username:"username"}     
+        const result = userView.createUser(playload)
+        expect(result.error).toMatch(/nesecitan tener un valor valido/)     
+        console.log(playload) 
     })
 })
